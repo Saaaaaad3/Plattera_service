e2cb0809e12da516ad28cb3553ed149fed107bda
Saad Mansuri <saadmansuri.work@gmail.com>
2025-05-29 06:05:43 +0530

    Added Basic Crud

--- prisma/migrations/20250518013954_decimal_place_fix_for_item_price/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `itemPrice` on the `MenuItems` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
-
-*/
--- AlterTable
-ALTER TABLE "MenuItems" ALTER COLUMN "itemPrice" SET DATA TYPE DECIMAL(10,2);
